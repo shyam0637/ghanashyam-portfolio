588e2a8f1bb9b682087a028004c0663ad1762399
shyam0637 <ghanashyammandal6370@gmail.com>
2025-08-02 19:34:42 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/ghanashyam-portfolio/', // Replace with your repository name
+  plugins: [react()]
+});
